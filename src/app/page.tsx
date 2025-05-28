5bf160498e0bc7d1eb510fbcacca6d5e5b63cb67
Tayse Rosa <tayse_rosa@yahoo.com.br>
2025-05-28 08:00:52 -0300

    chore: Add Prettier and EsLint setup

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return <Button className="bg-red-500 items-center flex">Teste button Tayse Rosadsfs2</Button>;
+}
